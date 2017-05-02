a0b4adf03c6138fcf8c24449effc92a6fff220e9
David Miotti <david@muxumuxu.com>
2017-05-02 02:03:50 +0200

    Added Bathroom.at to get number at position

--- lib/bathroom.ex
+++ lib/bathroom.ex
@@ -1,9 +1,18 @@
 defmodule Bathroom do
-  def keypad do
-    [
-      [1, 2, 3],
-      [4, 5, 6],
-      [7, 8, 9]
-    ]
+
+  defmodule State do
+    defstruct position: {1, 1}
+  end
+
+  def at(pos) do
+    case pos do
+      {x, _} when x < 0 or x > 2 -> :none
+      {_, y} when y < 0 or y > 2 -> :none
+      {x, y} -> elem(elem(keypad(), x), y)
+    end
+  end
+
+  defp keypad do
+    { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} }
   end
 end
--- test/bathroom_test.exs
+++ test/bathroom_test.exs
@@ -1,8 +1,21 @@
 defmodule BathroomTest do
   use ExUnit.Case
-  doctest BathroomSecurity
+  doctest Bathroom
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "Bathroom.at" do
+    assert Bathroom.at({0, 0}) == 1
+    assert Bathroom.at({0, 1}) == 2
+    assert Bathroom.at({0, 2}) == 3
+    assert Bathroom.at({1, 0}) == 4
+    assert Bathroom.at({1, 1}) == 5
+    assert Bathroom.at({1, 2}) == 6
+    assert Bathroom.at({2, 0}) == 7
+    assert Bathroom.at({2, 1}) == 8
+    assert Bathroom.at({2, 2}) == 9
+  end
+
+  test ":none when out of bound" do
+    assert Bathroom.at({-1, 0}) == :none
+    assert Bathroom.at({5, 1}) == :none
   end
 end
