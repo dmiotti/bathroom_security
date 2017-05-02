dd4d790086f3969686a670eb7060cf48a923330d
David Miotti <david@muxumuxu.com>
2017-05-02 02:08:30 +0200

    Added @keyboard instead of a function

--- lib/bathroom.ex
+++ lib/bathroom.ex
@@ -1,18 +1,12 @@
 defmodule Bathroom do
 
-  defmodule State do
-    defstruct position: {1, 1}
-  end
+  @keypad { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} }
 
   def at(pos) do
     case pos do
       {x, _} when x < 0 or x > 2 -> :none
       {_, y} when y < 0 or y > 2 -> :none
-      {x, y} -> elem(elem(keypad(), x), y)
+      {x, y} -> elem(elem(@keypad, x), y)
     end
-  end
-
-  defp keypad do
-    { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} }
   end
 end
